ce71c0cd49cbd4d4ee0387193ea6baf939bad99c
Carl Cassar <carlcassar@gmail.com>
2018-02-22 14:40:08 +0000

    Refactor FormController

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -34,7 +34,66 @@ class FormController extends Controller
      */
     public function store(StoreFormRequest $request)
     {
-        $user = User::create([
+        $user = $this->saveUserDetails($request);
+
+        $this->saveDifferentSelfieVersions($request, $user);
+
+        return $this->correctResponse($request, 'All user details were saved successfully');
+    }
+
+    /**
+     * @param        $user
+     * @param        $file
+     *
+     * @param string $type
+     *
+     * @return string
+     */
+    protected function makePath($user, $file, $type = 'original'): string
+    {
+        return 'img/selfies/' . $user->id . '_' . $type . '.' . $file->extension();
+    }
+
+    /**
+     * @param \App\Http\Requests\StoreFormRequest $request
+     *
+     * @param string                              $message
+     *
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    protected function correctResponse(StoreFormRequest $request, string $message)
+    {
+        if ($request->expectsJson($request)) {
+            return response($message);
+        }
+
+        return back()->with('success', $message);
+    }
+
+    /**
+     * @param \App\Http\Requests\StoreFormRequest $request
+     * @param                                     $user
+     */
+    protected function saveDifferentSelfieVersions(StoreFormRequest $request, $user): void
+    {
+        Image::make($file = $request->file('selfie'))
+            ->save($this->makePath($user, $file))
+            ->resize(1024, null, function (Constraint $constraint) {
+                $constraint->aspectRatio();
+            })
+            ->save($this->makePath($user, $file, 'max_width_1024'))
+            ->crop(150, 150)
+            ->save($this->makePath($user, $file, 'cropped'));
+    }
+
+    /**
+     * @param \App\Http\Requests\StoreFormRequest $request
+     *
+     * @return mixed
+     */
+    protected function saveUserDetails(StoreFormRequest $request)
+    {
+        return User::create([
             'first_name'           => $request->get('first_name'),
             'last_name'            => $request->get('last_name'),
             'email'                => $request->get('email'),
@@ -48,31 +107,5 @@ class FormController extends Controller
             'marvel_universe_live' => $request->has('marvel_universe_live'),
             'monster_jam'          => $request->has('monster_jam'),
         ]);
-
-        Image::make($file = $request->file('selfie'))
-            ->save($this->makePath($user, $file))
-            ->resize(1024, null, function (Constraint $constraint) {
-                $constraint->aspectRatio();
-            })
-            ->save($this->makePath($user, $file, 'max_width_1024'))
-            ->crop(150, 150)
-            ->save($this->makePath($user, $file, 'cropped'));
-
-        if ($request->expectsJson()) {
-            return response('All user details were saved successfully');
-        }
-
-        return back()->with('success', 'All user details were saved successfully');
-    }
-
-    /**
-     * @param $user
-     * @param $file
-     *
-     * @return string
-     */
-    protected function makePath($user, $file, $type = 'original'): string
-    {
-        return 'img/selfies/' . $user->id . '_' . $type . '.' . $file->extension();
     }
 }
